7d55161cf9d42e7cb121bb244f88312813022168
khushiiiii24 <khushisogani24@gmail.com>
2025-03-07 15:49:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,131 @@
+import { questions } from "./questions.js";
+function processQuestions(arr) {
+    const output = arr.map((object) => {
+        object.incorrect_answers.push(object.correct_answer);
+        object.incorrect_answers = randomize(object.incorrect_answers);
+        return object;
+    });
+    return output;//return object; likhne ka matlab hai "Maine jo changes kiye hain, us modified object ko naya array mein daal do." Yeh zaroori hai taki .map() function har element ke liye modified object ko return kare aur final result mein aapko expected updated questions array mile.
+}
+
+function randomize(arr) {
+    const temp = [];
+    const final = [];
+    for (let i = 0; i < arr.length; i++) {
+        final.push(arr[randomValue(arr.length, temp)]);
+    }
+    return final;
+}
+function randomValue(len, temp) {
+    const value = Math.floor(Math.random() * len);
+    if (temp.includes(value)) {
+        return randomValue(len, temp);
+    }
+    else {
+        temp.push(value);
+        return value;
+    }
+}
+
+const questionPara = document.querySelector(".question");
+const timerPara = document.querySelector(".timer");
+const optionPara = document.querySelectorAll(".option");
+const startBtn = document.querySelector("#start");
+const option = document.querySelector(".options");
+const quiz = document.querySelector(".quiz");
+const name = document.querySelector("input");
+let userAnswers = [];
+let questionNumber = 0;
+let countdown = 5;
+const players = localStorage.getItem("players") !== NULL ? JSON.parse(localStorage.getItem("players")) : [];
+const randomizedQuestions = randomize(processQuestions(questions));
+console.log(randomizedQuestions);
+
+startBtn.addEventListener("click", startTheQuiz);
+
+function startTheQuiz() {
+    storeDataInLS();
+
+    startBtn.style.display = "none";//hiding btn after click
+
+    nextQuestion(questionNumber);//showing 1st que
+    timerPara.innerText = countdown;
+
+    const interval = setInterval(() => {
+        if (countdown === 1) {
+            if (questionNumber < randomizedQuestions.length - 1) {
+                countdown = 5;
+                timerPara.innerText = countdown;
+                nextQuestion(++questionNumber);
+            }
+            else {
+                clearInterval(interval);
+                displayScore();
+            }
+        }
+        else {
+            timerPara.innerText = --countdown;
+        }
+    }, 1000);
+}
+
+function storeDataInLS() {
+    const player = {
+        name: name.value,
+        date: new Date().toLocaleString(),
+        score: 0
+    };
+    players.push(player);
+    localStorage.setItem("players", JSON.stringify(players));
+
+}
+
+function nextQuestion(questionNumber) {
+    questionPara.innerText = randomizedQuestions[questionNumber].question
+    for (let i = 0; i < optionPara.length; i++) {
+        optionPara[i].innerText = randomizedQuestions[questionNumber].incorrect_answers[i];
+        resetOption(option);//remove green or blue color
+
+        option.addEventListener("click", storeUserAnswer);
+    }
+
+}
+function resetOption(option) {
+    if (option.classList.contains("correct_answer")) {
+        option.classList.remove("correct_answer");
+    }
+    else if (option.classList.contains("incorrect_answer")) {
+        option.classList.remove("incorrect_answer");
+    }
+}
+function storeUserAnswer(e) {
+    console.log(e.target.innerText);
+    userAnswers.push({
+        questionNumber: questionNumber, userAnswer: e.target.innerText
+    });
+    if (e.target.innerText === randomizedQuestions[questionNumber].correct_answer) {
+        e.target.classList.add("correct_answer");
+        score++;
+    }
+    else {
+        e.target.classList.add("incorrect_answer");
+    }
+
+}
+
+function displayScore() {
+    quiz.style.display = "none";
+    const scoreDiv = document.createElement("div");
+    scoreDiv.classList.add("scoreDiv");
+
+    const h2 = document.createElement("h2");
+    h2.innerText = "your score is" + score;
+
+    scoreDiv.append(h2);
+    document.querySelector("#quizSection").append(scoreDiv);
+}
+
+
+
+
+
---
+++ questions.js
@@ -0,0 +1,95 @@
+export const questions = [
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question:
+      "This movie contains the quote, &quot;I feel the need ... the need for speed!&quot;",
+    correct_answer: "Top Gun",
+    incorrect_answers: ["Days of Thunder", "The Color of Money", "Cocktail"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question: "In &quot;ALF&quot;, what was ALF&#039;s real name?",
+    correct_answer: "Gordon Shumway",
+    incorrect_answers: ["Gordon Milipp", "Gordon Foster", "Gordon von Gam"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question:
+      "When does &quot;Rogue One: A Star Wars Story&quot; take place chronologically in the series?",
+    correct_answer: "Between Episode 3 and 4",
+    incorrect_answers: [
+      "After Episode 6",
+      "Before Episode 1",
+      "Between Episode 4 and 5",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question:
+      "Who in Pulp Fiction says &quot;No, they got the metric system there, they wouldn&#039;t know what the f*** a Quarter Pounder is.&quot;",
+    correct_answer: "Vincent Vega",
+    incorrect_answers: ["Jules Winnfield", "Jimmie Dimmick", "Butch Coolidge"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question: "Who plays Alice in the Resident Evil movies?",
+    correct_answer: "Milla Jovovich",
+    incorrect_answers: ["Madison Derpe", "Milla Johnson", "Kim Demp"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question: "In the Super Mario Bros. Movie (2023), who plays Toad?",
+    correct_answer: "Keegan-Michael Key",
+    incorrect_answers: ["Seth Rogan", "Charlie Day", "Jack Black"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question: "Who directed the Kill Bill movies?",
+    correct_answer: "Quentin Tarantino",
+    incorrect_answers: [
+      "Arnold Schwarzenegger",
+      "David Lean",
+      "Stanley Kubrick",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question:
+      "The 2002 film &quot;28 Days Later&quot; is mainly set in which European country?",
+    correct_answer: "United Kingdom",
+    incorrect_answers: ["France", "Italy", "Germany"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question:
+      "Which of these actors/actresses is NOT a part of the cast for the 2016 movie &quot;Suicide Squad&quot;?",
+    correct_answer: "Scarlett Johansson",
+    incorrect_answers: ["Jared Leto", "Will Smith", "Margot Robbie"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Film",
+    question: "Which of these movies did Jeff Bridges not star in?",
+    correct_answer: "The Hateful Eight",
+    incorrect_answers: ["Tron: Legacy", "The Giver", "True Grit"],
+  },
+];
